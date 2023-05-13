bd0814900723cb44e9940d3d09c4dc8d10edad57
Vpanwar-code <vivekpanwar183@gmail.com>
2023-05-14 02:06:17 +0530

    adding change amount button

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -1,11 +1,19 @@
+import React , {useState} from 'react';
 import './ExpenseItem.css';
 
 const ExpenseDetails = (props) => {
 
+        const [amount , setAmount]=useState(props.amount);
+
+        const handleAmount = () =>{
+            setAmount(100);
+        }
+
     return(
          <div className="expense-item__description">
-    <h2 className="expense-item h2">{props.title}</h2>
-    <div className="expense-item__price">${props.amount}</div>
+   
+    <div className="expense-item__price">${amount}</div>
+    <button onClick={handleAmount}>Change Amount</button>
 </div>
 );
    
