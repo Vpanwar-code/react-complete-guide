da67d165056293a73fd2c3d9cd5385dea93fc391
Vpanwar-code <vivekpanwar183@gmail.com>
2023-05-15 15:30:49 +0530

    adding submitting form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,22 +1,39 @@
-import React from 'react';
+
+import React, {useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () =>{
 
+    const [enteredTitle,setEnteredTitle]=useState('');
+    const [enteredAmount,setEnteredAmount]=useState('');
+    const [enteredDate,setEnteredDate]=useState('');
+
+
     const titleChangeHandler = (event) =>{
-                    console.log(event.target.value);
+        setEnteredTitle(event.target.value);
     }
 
      const amountChangeHandler = (event) =>{
-                    console.log(event.target.value);
+        setEnteredAmount(event.target.value);
     }
 
      const dateChangeHandler = (event) =>{
-                    console.log(event.target.value);
+        setEnteredDate(event.target.value);
     }
 
+    const submitHandler=(event)=>{
+        event.preventDefault();
+
+        const expenseData={
+            title:enteredTitle,
+            amount:enteredAmount,
+            date:new Date(enteredDate)
+        };
+         console.log(expenseData);
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
              <div>
             <div className='new-expense__control'>
                 <label>Title</label>
