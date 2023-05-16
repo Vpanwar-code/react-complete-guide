e418ee66e399f9183f80683220b502f44abecf35
Vpanwar-code <vivekpanwar183@gmail.com>
2023-05-16 16:30:07 +0530

    adding child to parent composition

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,7 +2,7 @@
 import React, {useState} from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () =>{
+const ExpenseForm = (props) =>{
 
     const [enteredTitle,setEnteredTitle]=useState('');
     const [enteredAmount,setEnteredAmount]=useState('');
@@ -29,7 +29,10 @@ const ExpenseForm = () =>{
             amount:enteredAmount,
             date:new Date(enteredDate)
         };
-         console.log(expenseData);
+         props.onSaveExpenseData(expenseData);
+         setEnteredTitle('');
+         setEnteredAmount('');
+         setEnteredDate('');
     };
 
     return (
@@ -37,15 +40,15 @@ const ExpenseForm = () =>{
              <div>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler}/>
+                <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                <input type='number' min="0.01" step="0.01" value ={enteredAmount} onChange={amountChangeHandler}/>
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min="2021-01-01" step="2022-12-31" onChange={dateChangeHandler}/>
+                <input type='date' min="2021-01-01" step="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className='new-expense__actions'>
